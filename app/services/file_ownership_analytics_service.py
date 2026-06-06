f5f4be00e95a78f2a2f25d7c5118c63e474c5525
unknown <sagaradmane6@gmail.com>
2026-06-06 18:34:28 +0530

    Modified file ownership service

--- app/services/file_ownership_analytics_service.py
+++ app/services/file_ownership_analytics_service.py
@@ -8,11 +8,35 @@ class FileOwnershipAnalyticsService:
     def get_file_ownership(db:Session, repo_id: int):
         file_owners = FileOwnershipAnalytics.get_file_ownership(db, repo_id)
 
-        return [
-            FileOwnershipAnalyticsSchema(
-                file_path=file_owner.path,
-                owner=file_owner.owner,
-                changes=file_owner.changes
-            )
-            for file_owner in file_owners
-        ]
+        # return [
+        #     FileOwnershipAnalyticsSchema(
+        #         file_path=file_owner.path,
+        #         owner=file_owner.owner,
+        #         changes=file_owner.changes
+        #     )
+        #     for file_owner in file_owners
+        # ]
+
+        owner_count = {}
+
+        for file_owner in file_owners:
+            owner = file_owner.owner
+
+            owner_count[owner] = owner_count.get(owner, 0) + 1
+
+
+        total_files = sum(owner_count.values())
+
+        result = []
+
+        for owner, count in owner_count.items():
+
+            percentage = round((count/total_files) * 100, 2)
+
+            result.append({
+                "owner": owner,
+                "owned_files": count,
+                "percentage": percentage
+            })
+
+        return result
