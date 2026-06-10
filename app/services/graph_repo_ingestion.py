a9c09d3aa223b36fd4c1350a187adbd883014b31
unknown <sagaradmane6@gmail.com>
2026-06-10 06:47:56 +0530

    Added a repo in neo4j

---
+++ app/services/graph_repo_ingestion.py
@@ -0,0 +1,22 @@
+from app.graph.graph_repository import GraphRepository
+
+from sqlalchemy.orm import Session
+
+from app.models.repository import Repository
+class GraphRepoIngestion:
+
+    def __init__(self):
+        self.graph_repo = GraphRepository()
+
+    def ingest_repos(self, db: Session):
+        repos = db.query(Repository).all()
+
+        for repo in repos:
+            self.graph_repo.create_repository_node(repo.id, repo.full_name)
+
+
+from app.db.session import SessionLocal
+service = GraphRepoIngestion()
+db = SessionLocal()
+
+service.ingest_repos(db)
